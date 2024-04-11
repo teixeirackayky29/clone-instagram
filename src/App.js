1f61164c9d30f73841636f7f3e273a5a96f4dfc3
Kayky Teixeira <95193248+teixeirackayky29@users.noreply.github.com>
2024-04-10 21:48:43 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import ConteudoLateralEsquerda from './componentes/ComponentesLateral';
+
+function App() {
+  return (
+    <>
+      <ConteudoLateralEsquerda/>
+
+      <div className="conteudoCentral">
+
+      </div>
+
+      <div className="conteudoLateralDireita">
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/ComponentesLateral.js
@@ -0,0 +1,16 @@
+import logo from '../assets/imagens/instagram.png';
+
+export default function ConteudoLateralEsquerda(){
+    return(
+        <>
+            <div className="conteudoLateralEsquerda">
+                <div className="logoInstagram">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="menu">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
